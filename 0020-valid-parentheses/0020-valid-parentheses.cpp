de8c532f46b54e640e04f7b3e5c8bffbccdf02cc
Suhani Chawla <suhanichawla47@gmail.com>
2026-08-22 18:51:07 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char> st;
+      for (int i=0;i<s.size();i++){
+        char c=s[i];
+        if(c=='('||c=='{'||c=='['){
+            st.push(c);
+        }
+        else if (c==')'){
+            if(!st.empty() && st.top()=='('){
+                st.pop();
+            }
+            else{
+                return false;
+            }}
+        else if (c=='}'){
+            if(!st.empty() && st.top()=='{'){
+                st.pop();
+            }
+            else{
+                return false;
+            }}
+        else if (c==']'){
+            if(!st.empty() && st.top()=='[' ){
+                st.pop();
+            }
+            else{
+                return false;
+            }}
+        else{
+            return false;
+        }
+       }
+     return st.empty(); }
+    
+    
+};
